adcce3e21d0590bd9991a1215ce8d15ad900cd73
PritalRupnur <pritalrupnur29012000@gmail.com>
2022-12-18 21:46:59 +0530

    latest update

--- server.js
+++ server.js
@@ -7,15 +7,24 @@ const expressLayout = require("express-ejs-layouts")
 const path=require('path')
 const PORT = process.env.PORT || 3000
 
+app.use(express.static('public'))
 
+
+
+//set template engine
+app.use(expressLayout)
+app.set("views", path.join(__dirname, 'resources/views'))
+app.set("view engine","ejs")
 
 app.get('/', (req, res) => {
     res.render("home")
 })
 
-app.use(expressLayout)
-app.set("views", path.join(__dirname, 'resources/views'))
-app.set("view engine","ejs")
+app.get('/cart',(req,res)=>{
+
+    res.render('coustmers/cart')
+})
+
 app.listen(PORT, (req, res) => {
     console.log(`Express App Running on ${PORT}`)
 })
